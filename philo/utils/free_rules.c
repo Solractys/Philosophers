691fd6d0e0267af2d34db41eef1c7cb69f80ac2b
Solractys <csilva-s@student.42sp.org.br>
2026-06-06 14:57:53 -0300

    Fix: Segfault on free function.

--- philo/utils/free_rules.c
+++ philo/utils/free_rules.c
@@ -5,21 +5,32 @@ void	destroy_forks(t_rule *rules)
 	int	i;
 
 	i = 0;
+	if (!rules->philos[0].left_fork)
+		return ;
 	while (i < rules->number_of_philosophers)
 	{
-		pthread_mutex_destroy(&rules->forks[i].mutex);
+		pthread_mutex_destroy(&rules->philos[i].left_fork->mutex);
+		free(rules->philos[i].left_fork);
 		i++;
 	}
-	free(rules->forks);
+}
+
+void	free_philosophers(t_rule *rules)
+{
+	if (!rules)
+		return ;
+	if (rules->philos)
+	{
+		destroy_forks(rules);
+		free(rules->philos);
+	}
 }
 
 void	free_rules(t_rule *rules)
 {
 	if (!rules)
 		return ;
-	if (rules->forks)
-		destroy_forks(rules);
 	if (rules->philos)
-		free(rules->philos);
+		free_philosophers(rules);
 	free(rules);
 }
